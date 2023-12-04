87f11b85c12316096a94137ae0c371971b1d8e23
masaaki630 <134359087+masaaki630@users.noreply.github.com>
2023-12-04 16:13:49 +0900

    ハンバーガーメニュー完成

--- kinoshita/js/hamburger.js
+++ kinoshita/js/hamburger.js
@@ -1,4 +1,5 @@
 document.querySelector('.hamburger').addEventListener('click', function(){
     this.classList.toggle('active');
     document.querySelector('.slide_menu').classList.toggle('hidden');
+    document.querySelector('.background').classList.toggle('hidden');
   })
