320b6603b646047c04d571734e8ebe60b1b4f99f
NishidaKensei <2201377@s.asojuku.ac.jp>
2023-12-04 12:17:09 +0900

    12/4　変更

--- kinoshita/checked/System_manage.php
+++ kinoshita/checked/System_manage.php
@@ -9,8 +9,7 @@
 <body>
     <header>
         管理画面
-        <a href="logout.html" value="ログアウト">ログアウト</a>
-    </header>
+        <a href="{%controller -echo%}logout.php" id="logout">ログアウト</a>    </header>
     <main>
     <div class="container">
         <div class="title-box">
